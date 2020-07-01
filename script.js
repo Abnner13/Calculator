4bd6715f167c236fa4c52381577e4c6288abae20
Abnner13 <Abnnerdavi019@gmail.com>
2020-07-01 09:42:45 -0300

    Calculator is working but, surplus operator problem

---
+++ script.js
@@ -0,0 +1,81 @@
+getHistory = () => document.getElementById('historyValue').innerText;
+
+function printHistory(num) {
+    document.getElementById('historyValue').innerText = num;
+}
+getOutput = () => document.getElementById('outputValue').innerText;
+
+function printOutput(num) {
+    if(num == ""){
+        document.getElementById('outputValue').innerHTML=num;
+    }else {
+        document.getElementById('outputValue').innerText=getFormattedNumber(num);
+    }
+}
+
+
+function getFormattedNumber(num) {
+    if (num == "-") {
+        return ""
+    }
+    var n = Number(num);
+    var value = n.toLocaleString("en");
+    return value;
+}
+
+reverseNumberFormat = (num) => Number(num.replace(/,/g,''));
+
+var operator = document.getElementsByClassName("Operators");
+for (let i = 0; i < operator.length; i++) {
+    operator[i].addEventListener('click', function () {
+        if(this.id == "Clean") {
+            printHistory("");
+            printOutput("");
+        }
+        else if(this.id == "BackSpace") {
+             var output = reverseNumberFormat(getOutput()).toString();
+             if(output){ //se output tiver valor
+                 output = output.substr(0, output.length-1);
+                 printOutput(output);
+             }
+        }
+        else{
+            var output = getOutput();
+            var history = getHistory();
+            if (output==""&&history!="") {
+                if(isNaN(history[history.length-1])){
+                    history = history.substr(0, history.length-1);
+                }
+            }
+            if(output!="" || history!=""){
+                output= output="" ?
+                output:reverseNumberFormat(output);
+                history=history+output;
+                if (this.id=="=") {
+                    let result = eval(history);
+                    printOutput(result);
+                    printHistory("");
+                }else {
+                    history=history+this.id;
+                    printHistory(history);
+                    printOutput("");
+                }
+            }
+        }
+
+    });
+}
+
+var number = document.getElementsByClassName("Numbres");
+for (var i = 0; i < number.length; i++) {
+    number[i].addEventListener('click', function () {
+        var output = reverseNumberFormat(getOutput());
+        if(output!=NaN){
+            output= output + this.id;
+            printOutput(output);
+        }
+    });
+}
+
+
+
